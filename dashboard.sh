cb92859af6ca26966fb9fd6a6cce37ca5abf4ce7
Teslim Otega Olunlade <tolunlade@fb.com>
2019-07-16 10:54:25 -0700

    Addition of a parse dashboard launch script and a placeholder '.env' file for the MASTER_KEY environment variable

---
+++ dashboard.sh
@@ -0,0 +1,7 @@
+ENV="/.env"
+if [ -f "$ENV" ]; then
+    source $ENV
+    parse-dashboard --appId kan-banana --masterKey $PARSE_MASTER_KEY --serverURL "https://kan-banana.herokuapp.com/parse"
+else
+    echo "Please create $ENV and set PARSE_MASTER_KEY"
+fi
