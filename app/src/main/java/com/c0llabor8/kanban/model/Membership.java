7050cf223ef6892fe295ded76fa3bd2c9886789f
alissahsueh <alissahsueh0313@berkeley.edu>
2019-07-17 14:26:39 -0700

    message and membership model added

--- app/src/main/java/com/c0llabor8/kanban/model/Membership.java
+++ app/src/main/java/com/c0llabor8/kanban/model/Membership.java
@@ -1,5 +1,33 @@
 package com.c0llabor8.kanban.model;
 
-public class Membership {
+
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+@ParseClassName("Membership")
+public class Membership extends ParseObject {
+
+  public static final String KEY_USER = "user";
+  public static final String KEY_PROJECT = "project";
+
+  public ParseUser getUser() {
+    return getParseUser(KEY_USER);
+  }
+
+  public void setUser(ParseUser user) {
+    put(KEY_USER, user);
+  }
+
+  public ParseObject getProject() {
+    return getParseObject(KEY_PROJECT);
+  }
+
+  public void setProject(String project) {
+    put(KEY_PROJECT, project);
+  }
+
+
 
 }
