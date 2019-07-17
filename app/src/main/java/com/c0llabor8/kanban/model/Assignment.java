26aff6435da74759983766af46c40ecae5bf6345
Monse Medel Martinez <bemedel@fb.com>
2019-07-17 14:26:34 -0700

    Model Assignment, Project and task

--- app/src/main/java/com/c0llabor8/kanban/model/Assignment.java
+++ app/src/main/java/com/c0llabor8/kanban/model/Assignment.java
@@ -1,5 +1,25 @@
 package com.c0llabor8.kanban.model;
 
-public class Assignment {
+import com.parse.ParseObject;
+import com.parse.ParseUser;
 
+public class Assignment extends ParseObject {
+  public static final String KEY_USER = "user";
+  public static final String KEY_TASK = "task";
+
+  public ParseUser getUser() {
+    return getParseUser(KEY_USER);
+  }
+
+  public void setUser(ParseUser user) {
+    put(KEY_USER, user);
+  }
+
+  public ParseObject getTask() {
+    return getParseObject(KEY_TASK);
+  }
+
+  public void setTask(ParseObject task) {
+    put(KEY_TASK, task);
+  }
 }
--- app/src/main/java/com/c0llabor8/kanban/model/Project.java
+++ app/src/main/java/com/c0llabor8/kanban/model/Project.java
@@ -1,5 +1,44 @@
 package com.c0llabor8.kanban.model;
 
-public class Project {
+import com.parse.ParseObject;
+import java.util.Date;
 
+public class Project extends ParseObject {
+
+  public static final String KEY_NAME = "name";
+  public static final String KEY_MEMBERS = "members";
+  public static final String KEY_TASKS = "tasks";
+  public static final String KEY_DEADLINE = "deadline";
+
+
+  public String getName() {
+    return getString(KEY_NAME);
+  }
+  public void setTitle(String name) {
+    put(KEY_NAME, name);
+  }
+
+  public int getMembers() {
+    return getInt(KEY_MEMBERS);
+  }
+
+  public void setMembers(String members) {
+    put(KEY_MEMBERS, members);
+  }
+
+  public int getTasks() {
+    return getInt(KEY_TASKS);
+  }
+
+  public void setTasks(String estimate) {
+    put(KEY_TASKS, estimate);
+  }
+
+  public Date getDeadline() {
+    return getDate(KEY_DEADLINE);
+  }
+
+  public void setDeadline(String deadline) {
+    put(KEY_DEADLINE, deadline);
+  }
 }
