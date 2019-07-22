ef2d2ad1fc2e8260b13f61ad015aac86fb11ed69
alissahsueh <alissahsueh0313@berkeley.edu>
2019-07-22 14:29:53 -0700

    correct time displays now

--- app/src/main/java/com/c0llabor8/kanban/adapter/TaskListAdapter.java
+++ app/src/main/java/com/c0llabor8/kanban/adapter/TaskListAdapter.java
@@ -1,12 +1,18 @@
 package com.c0llabor8.kanban.adapter;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import com.c0llabor8.kanban.databinding.ListItemTaskBinding;
 import com.c0llabor8.kanban.model.Task;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
+import java.util.TimeZone;
 
 public class TaskListAdapter extends RecyclerView.Adapter<TaskListAdapter.ViewHolder> {
 
@@ -20,6 +26,7 @@ public class TaskListAdapter extends RecyclerView.Adapter<TaskListAdapter.ViewHo
   @Override
   public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
     LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+
 
     ListItemTaskBinding binding =
         ListItemTaskBinding.inflate(inflater, parent, false);
@@ -50,8 +57,30 @@ public class TaskListAdapter extends RecyclerView.Adapter<TaskListAdapter.ViewHo
     void update(Task task) {
       binding.tvTitle.setText(task.getTitle());
       binding.tvDescription.setText(task.getDescription());
-      binding.tvEstimate.setText(Long.toString(task.getEstimate()));
+
+      binding.tvEstimate.setText(updateTime(task.getEstimate()));
     }
 
   }
+
+  public String updateTime(Long unixTime) {
+    Date date = new Date(unixTime);
+    String format = "MM/dd/yy";
+    SimpleDateFormat sdf = new SimpleDateFormat(format, Locale.getDefault());
+    sdf.setTimeZone(TimeZone.getDefault());
+    String formattedDate = sdf.format(date);
+    return formattedDate;
+  }
+
+  /**
+   * long unixSeconds = 1372339860;
+   * // convert seconds to milliseconds
+   * Date date = new java.util.Date(unixSeconds*1000L);
+   * // the format of your date
+   * SimpleDateFormat sdf = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss z");
+   * // give a timezone reference for formatting (see comment at the bottom)
+   * sdf.setTimeZone(java.util.TimeZone.getTimeZone("GMT-4"));
+   * String formattedDate = sdf.format(date);
+   * System.out.println(formattedDate);
+   */
 }
