afd2942d52120c04198a6b0605a52637ca84c61e
Monse Medel Martinez <bemedel@fb.com>
2019-07-29 12:27:17 -0700

    Modified the adapter

--- app/src/main/java/com/c0llabor8/kanban/adapter/CompletedTaskAdapter.java
+++ app/src/main/java/com/c0llabor8/kanban/adapter/CompletedTaskAdapter.java
@@ -1,11 +1,22 @@
 package com.c0llabor8.kanban.adapter;
 
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+import com.c0llabor8.kanban.model.Membership;
+import java.util.List;
 
 public class CompletedTaskAdapter extends RecyclerView.Adapter<CompletedTaskAdapter.ViewHolder> {
+
+  public CompletedTaskAdapter(List<Membership> memberList) {
+
+  }
+
+  public CompletedTaskAdapter(Context context, List<Membership> memberList) {
+
+  }
 
   @NonNull
   @Override
