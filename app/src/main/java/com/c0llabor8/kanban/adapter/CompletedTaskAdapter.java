591ca3d7cfded604653e2eddeb62694941a622b9
Monse Medel Martinez <bemedel@fb.com>
2019-07-29 12:13:45 -0700

    Added the adapter for Completed Tasks

--- app/src/main/java/com/c0llabor8/kanban/adapter/CompletedTaskAdapter.java
+++ app/src/main/java/com/c0llabor8/kanban/adapter/CompletedTaskAdapter.java
@@ -1,5 +1,33 @@
 package com.c0llabor8.kanban.adapter;
 
-public class CompletedTaskAdapter exte {
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 
+public class CompletedTaskAdapter extends RecyclerView.Adapter<CompletedTaskAdapter.ViewHolder> {
+
+  @NonNull
+  @Override
+  public CompletedTaskAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent,
+      int viewType) {
+    return null;
+  }
+
+  @Override
+  public void onBindViewHolder(@NonNull CompletedTaskAdapter.ViewHolder holder, int position) {
+
+  }
+
+  @Override
+  public int getItemCount() {
+    return 0;
+  }
+
+  public class ViewHolder extends RecyclerView.ViewHolder {
+
+    public ViewHolder(@NonNull View itemView) {
+      super(itemView);
+    }
+  }
 }
